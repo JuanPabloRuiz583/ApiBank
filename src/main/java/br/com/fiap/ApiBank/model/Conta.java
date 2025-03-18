b9113e40f2f3864e36c8fe9b701d474d9bf847f4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-18 20:45:18 -0300

    first commit

---
+++ src/main/java/br/com/fiap/ApiBank/model/Conta.java
@@ -0,0 +1,89 @@
+package br.com.fiap.ApiBank.model;
+
+import java.time.LocalDate;
+
+public class Conta {
+    private long id;
+    private String nomeTitular;
+    private String cpfTitular;
+    private String agencia;
+    private LocalDate dataAbertura;
+    private double saldo;
+    private boolean ativa = true;
+    private String tipo;
+
+    public Conta(long id, String nomeTitular, String cpfTitular, String agencia, LocalDate dataAbertura, double saldo, boolean ativa, String tipo) {
+        this.id = id;
+        this.nomeTitular = nomeTitular;
+        this.cpfTitular = cpfTitular;
+        this.agencia = agencia;
+        this.dataAbertura = dataAbertura;
+        this.saldo = saldo;
+        this.ativa = ativa;
+        this.tipo = tipo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public String getCpfTitular() {
+        return cpfTitular;
+    }
+
+    public void setCpfTitular(String cpfTitular) {
+        this.cpfTitular = cpfTitular;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public LocalDate getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDate dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
